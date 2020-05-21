897685014c4f141a31d589d833bba1d9cf2d77fb
JingqingLin <JingqingLin@live.com>
2020-05-21 15:40:16 +0800

    Update lab-1-encrypt.py

--- docs/course/cryptography/lab-1-encrypt.py
+++ docs/course/cryptography/lab-1-encrypt.py
@@ -1,55 +1,22 @@
 import sys
-import codecs
 
-MSGS = ["明文 1", "明文 2"]
+MSGS = ( ---  11 secret messages  --- )
 
-
-def split_hex(h, dtype='int'):
-    '''
-    :param h: A hex string. Example: "315c4eea". Its length should be an even number.
-    :return: A list of integer corresponding to every two letters in h
-    '''
-    if dtype == 'int':
-        split = [int(h[2 * i:2 * i + 2], 16) for i in range(len(h) // 2)]
-    elif dtype == 'str' or 'string':
-        split = [h[2 * i:2 * i + 2] for i in range(len(h) // 2)]
-    else:
-        split = [int(h[2 * i:2 * i + 2], 16) for i in range(len(h) // 2)]
-
-    return split
-
-
-def strxor(a, b):
-    # xor two strings of different lengths
+def strxor(a, b):     # xor two strings of different lengths
     if len(a) > len(b):
-        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
     else:
-        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
-
+       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
 def random(size=16):
     return open("/dev/urandom").read(size)
 
-
 def encrypt(key, msg):
     c = strxor(key, msg)
-    c = codecs.encode(bytes(c, encoding='utf-8'), 'hex')
+    print
+    print c.encode('hex')
     return c
-
-
-def decrypt(key, cipher):
-    cipher = ''.join([chr(x) for x in split_hex(cipher)])
-    msg = strxor(key, cipher)
-    return msg
-
 
 def main():
     key = random(1024)
     ciphertexts = [encrypt(key, msg) for msg in MSGS]
-    print(ciphertexts)
-    plaintexts = [decrypt(key, cipher) for cipher in ciphertexts]
-    print(plaintexts)
-
-
-if __name__ == "__main__":
-    main()
